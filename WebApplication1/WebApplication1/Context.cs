ee70c3359c38ef7206ff98c0db51c400256313ca
Robert Mikoś <s26294@pjwstk.edu.pl>
2024-05-26 19:40:44 +0200

    z zajec

---
+++ WebApplication1/WebApplication1/Context.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1
+{
+    public class Context : DbContext
+    {
+        public Context() { }
+        public Context(DbContextOptions<Context> options) : base(options) { }
+
+        public virtual DbSet<Medicament> Medicament { get; set; }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Patient.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models
+{
+    public class Patient
+    {
+        public int IdPatient { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BrirthDate { get; set; }
+        public virtual ICollection<Prescription> Prescriptions { get; set; } = new List<Prescription>();
+    }
+}
